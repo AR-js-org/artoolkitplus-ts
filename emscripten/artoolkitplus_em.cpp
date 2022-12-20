8d636e2429fd66d3596842df3590dd13df47c62c
Walter Perdan <github@kalwaltart.it>
2022-12-20 19:07:26 +0100

    new methods and enums: - methods: setUndistortionMode, setThreshold, selectBestMarkerByCf - enums: MARKER_MODE, UNDIST_MODE

--- emscripten/artoolkitplus_em.cpp
+++ emscripten/artoolkitplus_em.cpp
@@ -12,7 +12,11 @@ public:
             int maxLoadPatterns = 0)
       : TrackerSingleMarker(imWidth, imHeight, maxImagePatterns, pattWidth,
                             pattHeight, pattSamples, maxLoadPatterns) {}
-
+  std::vector<int> calc(uintptr_t nImage) {
+    auto ptrImg = reinterpret_cast<uint8_t *>(nImage);
+    return TrackerSingleMarker::calc(ptrImg);
+  }
+  float getConfidence() { return TrackerSingleMarker::getConfidence(); }
   bool init(std::string paramFile, ARFloat nearCLip, ARFloat nFarClip) {
     return TrackerSingleMarker::init(paramFile.c_str(), nearCLip, nFarClip);
   }
@@ -22,13 +26,22 @@ public:
   void printCameraSettings() {
     TrackerSingleMarker::getCamera()->printSettings();
   };
+  int selectBestMarkerByCf() {
+    return TrackerSingleMarker::selectBestMarkerByCf();
+  }
+  void setBorderWidth(ARFloat nFraction) {
+    TrackerSingleMarker::setBorderWidth(nFraction);
+  }
+  void setMarkerMode(MARKER_MODE nMarkerMode) {
+    TrackerSingleMarker::setMarkerMode(nMarkerMode);
+  };
   void setPatternWidth(float width) {
     TrackerSingleMarker::setPatternWidth(width);
   };
-  std::vector<int> calc(uintptr_t nImage) {
-    auto ptrImg = reinterpret_cast<uint8_t *>(nImage);
-    return TrackerSingleMarker::calc(ptrImg);
-  }
+  void setThreshold(int nValue) { TrackerSingleMarker::setBorderWidth(nValue); }
+  void setUndistortionMode(UNDIST_MODE nMode) {
+    TrackerSingleMarker::setUndistortionMode(nMode);
+  };
 };
 
 #include "bindings.cpp"
--- emscripten/bindings.cpp
+++ emscripten/bindings.cpp
@@ -6,19 +6,36 @@ EMSCRIPTEN_BINDINGS(artoolkitplus) {
   register_vector<int>("vector_int");
 
   enum_<PIXEL_FORMAT>("PIXEL_FORMAT")
-  .value("PIXEL_FORMAT_ABGR", PIXEL_FORMAT_ABGR)
-  .value("PIXEL_FORMAT_BGRA", PIXEL_FORMAT_BGRA)
-  .value("PIXEL_FORMAT_BGR", PIXEL_FORMAT_BGR)
-  .value("PIXEL_FORMAT_RGBA", PIXEL_FORMAT_RGBA)
-  .value("PIXEL_FORMAT_RGB", PIXEL_FORMAT_RGB)
-  .value("PIXEL_FORMAT_RGB565", PIXEL_FORMAT_RGB565)
-  .value("PIXEL_FORMAT_LUM", PIXEL_FORMAT_LUM);
+      .value("PIXEL_FORMAT_ABGR", PIXEL_FORMAT_ABGR)
+      .value("PIXEL_FORMAT_BGRA", PIXEL_FORMAT_BGRA)
+      .value("PIXEL_FORMAT_BGR", PIXEL_FORMAT_BGR)
+      .value("PIXEL_FORMAT_RGBA", PIXEL_FORMAT_RGBA)
+      .value("PIXEL_FORMAT_RGB", PIXEL_FORMAT_RGB)
+      .value("PIXEL_FORMAT_RGB565", PIXEL_FORMAT_RGB565)
+      .value("PIXEL_FORMAT_LUM", PIXEL_FORMAT_LUM);
+
+  enum_<UNDIST_MODE>("UNDIST_MODE")
+      .value("UNDIST_NONE", UNDIST_NONE)
+      .value("UNDIST_STD", UNDIST_STD)
+      .value("UNDIST_LUT", UNDIST_LUT);
+
+  enum_<MARKER_MODE>("MARKER_MODE")
+      .value("MARKER_TEMPLATE", MARKER_TEMPLATE)
+      .value("MARKER_ID_SIMPLE", MARKER_ID_SIMPLE)
+      .value("MARKER_ID_BCH", MARKER_ID_BCH);
 
   class_<TrackerSM>("TrackerSingleMarker")
       .constructor<int, int, int, int, int, int, int>()
+      .function("calc", &TrackerSM::calc, allow_raw_pointer<uint8_t>())
+      .function("getConfidence", &TrackerSM::getConfidence)
       .function("init", &TrackerSM::init)
       .function("printCameraSettings", &TrackerSM::printCameraSettings)
+      .function("selectBestMarkerByCf", &TrackerSM::selectBestMarkerByCf)
+      .function("setBorderWidth", &TrackerSM::setBorderWidth)
+      .function("setMarkerMode", &TrackerSM::setMarkerMode)
       .function("setPixelFormat", &TrackerSM::setPixelFormat)
       .function("setPatternWidth", &TrackerSM::setPatternWidth)
-      .function("calc", &TrackerSM::calc, allow_raw_pointer<uint8_t>());
+      .function("setThreshold", &TrackerSM::setThreshold)
+      .function("setUndistortionMode", &TrackerSM::setUndistortionMode);
+      
 };
