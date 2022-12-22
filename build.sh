3a67394db61c6277b05c645c9cb2263d9e4cbee7
Walter Perdan <github@kalwaltart.it>
2022-12-22 22:33:34 +0100

    adding more messages to the build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 cd build
+echo "Preparing Release libs.........."
+echo "Configuring with cmake"
 emcmake cmake .. -DCMAKE_BUILD_TYPE="Release"
+echo "Building the libs"
 emmake make
+echo "Preparing Debug libs..........."
+echo "Configuring with cmake"
 emcmake cmake .. -DCMAKE_BUILD_TYPE="Debug"
+echo "Building the libs"
 emmake make
