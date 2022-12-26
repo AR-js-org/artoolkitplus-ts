52fecaea46e53792fc35b2d2f4cc0e3d64cb6f32
Walter Perdan <github@kalwaltart.it>
2022-12-26 23:09:28 +0100

    rebuilding lib

--- src/abstractions/interfaces.ts
+++ src/abstractions/interfaces.ts
@@ -5,17 +5,3 @@ export interface IImageObj extends HTMLCanvasElement {
     height: number;
     data: Uint8ClampedArray;
   }
-
-  export interface IARToolkitplus {
-    setup: (
-        cameraUrl: string,
-        maxImagePatterns: number,
-        pattWidth: number,
-        pattHeight: number,
-        pattSamples: number,
-        maxLoadPatterns: number
-    ) => void;
-    update: (image: IImageObj) => void;
-    getConfidence: () => number;
-    getModelViewMatrix: () => Array<number>;
-  }
