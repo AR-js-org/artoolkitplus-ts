95a330773ef5a12046cb50d75c6e637cb22665bf
Walter Perdan <github@kalwaltart.it>
2023-02-09 15:39:19 +0100

    new addPattern function

--- types/src/TrackerSingleMarker.d.ts
+++ types/src/TrackerSingleMarker.d.ts
@@ -2,13 +2,18 @@ import { AbstractTrackerSingleMarker } from "./abstractions/AbstractTrackerSingl
 import { IImageObj } from "./abstractions/CommonInterfaces";
 export default class TrackerSingleMarker extends AbstractTrackerSingleMarker {
     private tracker;
+    private marker_count;
+    private FS;
     constructor(useBCH: boolean, cameraUrl: string, width: number, height: number, patternWidth: number);
     static initTrackerSingleMarker(useBCH: boolean, cameraUrl: string, width: number, height: number, patternWidth: number): Promise<any>;
     setup: (cameraUrl: string) => void;
-    update: (image: IImageObj) => void;
+    update: (image: IImageObj) => any;
     getConfidence: () => number;
     getModelViewMatrix: () => number[];
     getMarkerPos: () => object;
     getMarkerVertexes: () => Array<number>;
+    setMarkerMode: (markerMode: any) => void;
+    addPattern(urlOrData: any): Promise<any>;
     private initTSM;
+    private _storeDataFile;
 }
